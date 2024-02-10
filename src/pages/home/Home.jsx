33b9f7d7b010f5f059707f232bd4e6e575a04241
rezaaminiweb <rezaaminiweb@gmail.com>
2024-02-10 17:19:39 +0330

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import PersonIcon from '@mui/icons-material/Person';
+import Topbar from '../../components/topbar/topbar';
+const Home = ()=>{
+    return(
+        
+        <Topbar/>
+        
+    )
+}
+export default Home
